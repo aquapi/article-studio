2885324aebf508812b9998782bde2452166df496
Revenity <87530663+aquapi@users.noreply.github.com>
2021-11-22 16:35:59 +0700

    Server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import { server } from "./app/config.js";
+import { hostname, port } from "./resource/resource.js";
 import "./app/app.js";
 
-// Port 443
-server.listen(443);
+// Listen
+server.listen(port, hostname, () => {
+    console.log('Server is listening on port ' + port + ', on address https://' + hostname);
+});
