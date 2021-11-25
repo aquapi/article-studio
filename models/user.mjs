5ec55a809dbb8b61e814379caf2b96f18185ff16
Revenity <87530663+aquapi@users.noreply.github.com>
2021-11-25 09:14:22 +0700

    Models

--- models/user.mjs
+++ models/user.mjs
@@ -7,6 +7,6 @@ const userSchema = new mongoose.Schema({
 });
 
 // User models
-const User = mongoose.model("User", userSchema);  
+const User = mongoose.model("User", userSchema);
 
 export default User;
