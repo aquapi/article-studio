8c6ec4da63febdd82c37ff861b03ec9bcff0a631
Revenity <87530663+aquapi@users.noreply.github.com>
2021-11-28 21:22:45 +0700

    Routes

--- routes/content/read.mjs
+++ routes/content/read.mjs
@@ -1,57 +1,51 @@
 import { DB } from "../../resource/resource.mjs";
 import app from "../../app/config.mjs";
-import handlebars from "handlebars";
-import fs from "fs";
+import { next } from "../../app/next.mjs";
 
 let Csession;
 
 // Read for client
-app.get("/reader/:name", (req, res) => {
+app.get("/reader/:name", async (req, res) => {
     Csession = req.session;
     // Check whether '/reader' has been provided with a 'name' parameter
     if (!req.params || !req.params.name)
         res.redirect("/article");
-    DB.sites.findOne({
+    const r = await DB.sites.findOne({
         name: req.params.name
-    })
-        .then(r => {
-            if (!r) {
-                res.redirect("/article");
-                return;
-            }
-            let template = handlebars.compile(fs.readFileSync("./pages/article/read.html").toString());
-            res.write(template({
-                name: req.params.name,
-                admin_button: `
+    });
+    // If reader can't find target article
+    if (!r) {
+        res.redirect("/article");
+        return;
+    }
+    // If another user visits the read site 
+    if (r.user !== Csession.userID)
+        await DB.sites.replaceOne(r, {
+            user: r.user,
+            name: r.name,
+            content: r.content,
+            display_img: r.display_img,
+            description: r.description,
+            views: r.views + 1,
+            tag: r.tag,
+            votes: r.votes
+        }).catch(err => {
+            throw err;
+        });
+    // Render
+    return next.render(req, res, "/article/read", {
+        name: req.params.name,
+        admin_button: `
                 <button onclick='location.replace("/article/edit/${encodeURIComponent(r.name)}")' style="display: ${r.user === Csession.userID ? 'block' : 'none'};">Edit</button>
                 <button style="display: ${r.user === Csession.userID ? 'block' : 'none'};" id="del">Delete</button>
                 <button onclick='location.replace("/vote/${encodeURIComponent(r.name)}")' style="display: ${r.user === Csession.userID ? 'none' : 'block'};">Vote</button>
                 `,
-                content: r && r.content ? r.content : `This article have no content`,
-                views: r.views,
-                author: r.user,
-                tag: r.tag,
-                votes: r.votes
-            }));
-            if (r.user !== Csession.userID)
-                return DB.sites.replaceOne(r, {
-                    user: r.user,
-                    name: r.name,
-                    content: r.content,
-                    display_img: r.display_img,
-                    description: r.description,
-                    views: r.views + 1,
-                    tag: r.tag,
-                    votes: r.votes
-                });
-        })
-        .then(() => {
-            res.end();
-            return;
-        })
-        .catch(err => {
-            throw err;
-        });
+        content: r && r.content ? r.content : `This article have no content`,
+        views: r.views,
+        author: r.user,
+        tag: r.tag,
+        votes: r.votes
+    });
 });
 
 // Vote
