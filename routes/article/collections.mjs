6373714807936b5d4ca53a295efee85ed4bcb083
Revenity <87530663+aquapi@users.noreply.github.com>
2021-11-29 19:55:38 +0700

    Routes

--- routes/article/collections.mjs
+++ routes/article/collections.mjs
@@ -15,114 +15,123 @@ let Csession;
 // My article subpage: Show all articles created not by others
 // https://localhost/myarticle
 
-app.get("/myarticle", (req, res) => {
+app.get("/myarticle", async (req, res) => {
 	// Check whether the user is logged in
 	Csession = req.session;
 	if (!Csession.userID)
 		res.redirect("/login");
 	// Init webpage
 	res.write(fs.readFileSync("./pages/article/article.html"));
-	// add created article with name matched to webpage 
-	DB.sites.find({
+	// Search all articles which belongs to current user
+	const r = await DB.sites.find({
 		user: Csession.userID
-	})
-		.then(r => {
-			let script = "";
-			let article = [];
-			res.write(SortByComponent(true));
-			res.write(Header("My Articles"));
-			res.write("<div style='display: none; overflow-x: scroll; overflow-y: hidden, max-height: 504px' id='created-article'>");
-			for (let i of r) {
-				article.push({
-					content: ArticleTemplate(i),
-					views: i.views ? i.views : 0,
-					author: i.user
-				});
-				script += ScriptTemplate(i);
-			}
-
-			// Check whether article number is larger than 4
-			if (article.length > 4) {
-				res.write(`
-					<script>
-						document.querySelector("#created-article").style["justify-content"] = "flex-start";
-					</script>
-				`)
-			}
-			// Init articles
-			const length = article.length;
-			InitCategory("views", res, article);
-			res.write("</div></div>");
-
-			// Load javascript in webpage
-			InitLoginScreen(res, Csession);
-			res.write(`<script>${script}</script>`)
-			// Check if no article found
-			if (length === 0) {
-				res.write(`<script>
-                    location.replace("/article")
-                </script>`)
-			}
-			res.end(EndScript(Csession, "My Articles"));
-		})
-		.catch(err => {
-			throw err;
+	});
+	let script = "";
+	/**
+	 * @type {{content: string, views: number, author: string, votes: number}[]}
+	 */
+	let article = [];
+	// All lists of articles
+	res.write(SortByComponent(true));
+	// Header
+	res.write(Header("My Articles"));
+	// Articles
+	res.write("<div style='display: none; overflow-x: scroll; overflow-y: hidden, max-height: 504px' id='created-article'>");
+	for (let i of r) {
+		article.push({
+			content: ArticleTemplate(i),
+			views: i.views ? i.views : 0,
+			author: i.user
 		});
+		script += ScriptTemplate(i);
+	}
+
+	// Check whether article number is larger than 4
+	if (article.length > 4) {
+		res.write(`
+			<script>
+				document.querySelector("#created-article").style["justify-content"] = "flex-start";
+			</script>
+		`)
+	}
+	// Init articles
+	const length = article.length;
+	// Init articles
+	article = InitCategory("views", article);
+	for (let atc of article)
+		res.write(atc.content);
+	// Close the created article div
+	res.write("</div>");
+
+	// Load javascript in webpage
+	res.write(InitLoginScreen(Csession));
+	res.write(`<script>${script}</script>`)
+	// Check if no article found
+	if (length === 0) {
+		res.write(`<script>
+            location.replace("/article")
+        </script>`)
+	}
+	res.end(EndScript(Csession, "My Articles"));
 });
 
 // Other article subpage: Show all articles created by others
 // https://localhost/otherarticle
 
-app.get("/otherarticle", (req, res) => {
+app.get("/otherarticle", async (req, res) => {
 	// Check whether the user is logged in
 	Csession = req.session;
 	if (!Csession.userID)
 		res.redirect("/login");
 	// Init webpage
 	res.write(fs.readFileSync("./pages/article/article.html"));
-	// add created article with name matched to webpage
-	DB.sites.find({})
-		.then(r => {
-			let script = "";
-			let article = [];
-			res.write(SortByComponent(true));
-			res.write(Header("Other Articles"));
-			res.write("<div style='display: none; overflow-x: scroll; overflow-y: hidden, max-height: 504px' id='created-article'>");
-			for (let i of r) {
-				if (i.user !== Csession.userID) {
-					article.push({
-						content: ArticleTemplate(i),
-						views: i.views ? i.views : 0,
-						author: i.user
-					});
-					script += ScriptTemplate(i);
-				}
-			}
-			// Check whether article number is larger than 4
-			if (article.length > 4) {
-				res.write(`
-					<script>
-						document.querySelector("#created-article").style["justify-content"] = "flex-start";
-					</script>
-				`)
-			}
-			const length = article.length;
-			// Init articles
-			InitCategory("views", res, article);
-			res.write("</div></div>");
+	// Search all articles
+	const r = await DB.sites.find({});
+	let script = "";
+	/**
+	 * @type {{content: string, views: number, author: string, votes: number}[]}
+	 */
+	let article = [];
+	// All lists of articles
+	res.write(SortByComponent(true));
+	// Header
+	res.write(Header("Other Articles"));
+	// Articles
+	res.write("<div style='display: none; overflow-x: scroll; overflow-y: hidden, max-height: 504px' id='created-article'>");
+	for (let i of r) {
+		if (i.user !== Csession.userID) {
+			article.push({
+				content: ArticleTemplate(i),
+				views: i.views ? i.views : 0,
+				author: i.user
+			});
+			script += ScriptTemplate(i);
+		}
+	}
+	// Check whether article number is larger than 4
+	if (article.length > 4) {
+		res.write(`
+			<script>
+				document.querySelector("#created-article").style["justify-content"] = "flex-start";
+			</script>
+		`);
+	}
+	const length = article.length;
+	// Init articles
+	article = InitCategory("views", article);
+	for (let atc of article)
+		res.write(atc.content);
+	// Close created article div
+	res.write("</div>");
 
-			// Load javascript in webpage
-			InitLoginScreen(res, Csession);
-			res.write(`<script>${script}</script>`);
-			// Check if no article found
-			if (length === 0) {
-				res.write(`<script>
-                    location.replace("/article")
-                </script>`)
-			}
-			res.end(EndScript(Csession, "Other Articles"));
-		})
-		.catch(err => {
-			throw err;
-		});
+	// Load javascript in webpage
+	res.write(InitLoginScreen(Csession));
+	res.write(`<script>${script}</script>`);
+	// Check if no article found
+	if (length === 0) {
+		res.write(`<script>
+            location.replace("/article")
+        </script>`)
+	}
+	res.end(EndScript(Csession, "Other Articles"));
 });
--- routes/article/homepage.mjs
+++ routes/article/homepage.mjs
@@ -15,94 +15,104 @@ let Csession;
 // Homepage
 // https://localhost/article
 
-app.get("/article", (req, res) => {
+app.get("/article", async (req, res) => {
     // Init webpage
     Csession = req.session;
     res.write(fs.readFileSync("./pages/article/article.html").toString().trim().replace(/\<\/html\>/, "").replace(/\<\/body\>/, ""));
-    // add created article with name matched to webpage
-    DB.sites.find({})
-        .then(r => {
-            let script = "";
-            let article = [];
-            res.write(SortByComponent(!!Csession.userID))
-            res.write(Header("Discover"));
-            res.write("<div style='display: none; overflow-x: scroll; overflow-y: hidden, max-height: 504px' id='created-article'>");
-            for (let i of r) {
-                article.push({
-                    content: ArticleTemplate(i),
-                    views: i.views ? i.views : 0,
-                    author: i.user,
-                    votes: i.votes,
-                    data: i
-                });
-                script += ScriptTemplate(i);
-            }
-            // Check whether article number is larger than 4
-            if (article.length > 4) {
-                res.write(`
-					<script>
-						document.querySelector("#created-article").style["justify-content"] = "flex-start";
-					</script>
-				`)
-            }
-
-            // Init articles
-            InitCategory("views", res, article);
-            res.write("</div></div>");
-
-            // Load javascript in webpage
-            InitLoginScreen(res, Csession);
-            res.write(`<script>${script}</script>`);
-            res.end(EndScript(Csession, "Discover"));
-        })
-        .catch(err => {
-            throw err;
+    // Find all articles
+    const r = await DB.sites.find({});
+    let script = "";
+    /**
+     * @type {{content: string, views: number, author: string, votes: number}[]}
+     */
+    let article = [];
+    // All lists of articles link
+    res.write(SortByComponent(!!Csession.userID));
+    // Header
+    res.write(Header("Discover"));
+    // Created articles
+    res.write("<div style='display: none; overflow-x: scroll; overflow-y: hidden, max-height: 504px' id='created-article'>");
+    for (let i of r) {
+        article.push({
+            content: ArticleTemplate(i),
+            views: i.views ? i.views : 0,
+            author: i.user,
+            votes: i.votes
         });
+        script += ScriptTemplate(i);
+    }
+    // Check whether article number is larger than 4
+    if (article.length > 4) {
+        res.write(`
+			<script>
+				document.querySelector("#created-article").style["justify-content"] = "flex-start";
+			</script>
+		`)
+    }
+
+    // Init articles
+    article = InitCategory("views", article);
+    for (let atc of article)
+        res.write(atc.content);
+    // Close created article div
+    res.write("</div>");
+
+    // Load javascript in webpage
+    res.write(InitLoginScreen(Csession));
+    res.write(`<script>${script}</script>`);
+    res.end(EndScript(Csession, "Discover"));
 });
 
 // Most Vote
 // https://localhost/mostvote
 
-app.get("/mostvote", (req, res) => {
+app.get("/mostvote", async (req, res) => {
     // Init webpage
     Csession = req.session;
     res.write(fs.readFileSync("./pages/article/article.html").toString().trim().replace(/\<\/html\>/, "").replace(/\<\/body\>/, ""));
-    // add created article with name matched to webpage
-    DB.sites.find({})
-        .then(r => {
-            let script = "";
-            let article = [];
-            res.write(SortByComponent(!!Csession.userID))
-            res.write(Header("Most Voted"));
-            res.write("<div style='display: none; overflow-x: scroll; overflow-y: hidden, max-height: 504px' id='created-article'>");
-            for (let i of r) {
-                article.push({
-                    content: ArticleTemplate(i),
-                    views: i.views ? i.views : 0,
-                    author: i.user,
-                    votes: i.votes
-                });
-                script += ScriptTemplate(i);
-            }
-            // Check whether article number is larger than 4
-            if (article.length > 4) {
-                res.write(`
-					<script>
-						document.querySelector("#created-article").style["justify-content"] = "flex-start";
-					</script>
-				`)
-            }
-
-            // Init articles
-            InitCategory("votes", res, article);
-            res.write("</div></div>");
-
-            // Load javascript in webpage
-            InitLoginScreen(res, Csession);
-            res.write(`<script>${script}</script>`);
-            res.end(EndScript(Csession, "Most Voted"));
-        })
-        .catch(err => {
-            throw err;
+    // Search all articles
+    const r = await DB.sites.find({});
+    let script = "";
+    /**
+     * @type {{content: string, views: number, author: string, votes: number}[]}
+     */
+    let article = [];
+    // All lists of articles link
+    res.write(SortByComponent(!!Csession.userID))
+    // Header
+    res.write(Header("Most Voted"));
+    // Created article
+    res.write("<div style='display: none; overflow-x: scroll; overflow-y: hidden, max-height: 504px' id='created-article'>");
+    // Add articles from database
+    for (let i of r) {
+        article.push({
+            content: ArticleTemplate(i),
+            views: i.views ? i.views : 0,
+            author: i.user,
+            votes: i.votes
         });
-})
+        script += ScriptTemplate(i);
+    }
+    // Check whether article number is larger than 4
+    if (article.length > 4) {
+        res.write(`
+			<script>
+				document.querySelector("#created-article").style["justify-content"] = "flex-start";
+			</script>
+		`);
+    }
+
+    // Init articles
+    article = InitCategory("votes", article);
+    for (let atc of article)
+        res.write(atc.content);
+    // End of articles block
+    res.write("</div>");
+
+    // Load javascript in webpage
+    res.write(InitLoginScreen(Csession));
+    // All scripts
+    res.write(`<script>${script}</script>`);
+    // End scripts
+    res.end(EndScript(Csession, "Most Voted"));
+});
