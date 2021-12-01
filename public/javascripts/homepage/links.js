7736537d9e8f900b28bace016de2095f1d9c395b
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-01 19:25:07 +0700

    Stylesheets, Images

---
+++ public/javascripts/homepage/links.js
@@ -0,0 +1,2 @@
+const names = document.querySelectorAll("span").item(2).innerHTML;
+names.split(",").forEach(e => document.getElementById(e).addEventListener("click", () => location.replace(`/reader/${encodeURIComponent(e)}`)));
