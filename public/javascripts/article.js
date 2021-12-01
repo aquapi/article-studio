2408dbb4bb9e245d5c12e8983ea04763a87ee884
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-01 14:11:24 +0700

    Stylesheets, Images

--- public/javascripts/article.js
+++ public/javascripts/article.js
@@ -74,3 +74,7 @@ $("#inner").click(e => {
 // Resize window
 initWidthHeight();
 onresize = initWidthHeight;
+
+// If articles count larger than 4
+if (document.getElementsByClassName("created").length > 4) 
+    document.querySelector("#created-article").style["justify-content"] = "flex-start";
