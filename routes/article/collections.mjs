f5ca302ab2b04e0cff57d99318a0a3a28a76087a
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-01 19:53:13 +0700

    Routes

--- routes/article/collections.mjs
+++ routes/article/collections.mjs
@@ -1,11 +1,8 @@
 import {
 	DB,
-	ArticleTemplate,
 	InitCategory,
-	Header,
-	SortByComponent
 } from "../../resource/resource.mjs";
-import fs from "fs";
+import { next } from "../../app/next.mjs";
 import app from "../../app/config.mjs";
 
 let Csession;
@@ -24,26 +21,12 @@ app.get("/myarticle", async (req, res) => {
 	// Check whether the user is logged in
 	else if (!Csession.userID)
 		res.redirect("/login");
-	// Init webpage
-	res.write(fs.readFileSync("./pages/article/article.html"));
 	/**
 	 * @type {{name: string, content: string, views: number, author: string, votes: number}[]}
 	 */
 	let article = [];
-	// Init hidden data
-	res.write(`
-        <span style="display: none">${Csession.userID}</span>
-        <span style="display: none">My Articles</span>
-    `)
-	// All lists of articles
-	res.write(SortByComponent(true));
-	// Header
-	res.write(Header("My Articles"));
-	// Articles
-	res.write("<div id='created-article'>");
 	for (let i of r) {
 		article.push({
-			content: ArticleTemplate(i),
 			views: i.views ?? 0,
 			author: i.user,
 			name: i.name,
@@ -52,17 +35,11 @@ app.get("/myarticle", async (req, res) => {
 	}
 	// Init articles
 	article = InitCategory("views", article);
-	for (let atc of article)
-		res.write(atc.content);
-	// Close the created article div
-	res.write("</div>");
-	res.write(`
-        <span style="display: none">${article.map(e => e.name)}}</span>
-    `)
-    // Load javascript in webpage
-    res.write(`<script src="/javascripts/homepage/links.js"></script>`)
-	res.write(`<script src="/javascripts/homepage/navbuttons.js"></script>`);
-	res.end('<script src="/javascripts/homepage/endscript.js"></script>');
+	return next.render(req, res, "/article/article", {
+        Csession: Csession,
+        headerName: "My Article",
+        articles: article
+    });
 });
 
 // Other article subpage: Show all articles created by others
@@ -80,27 +57,13 @@ app.get("/otherarticle", async (req, res) => {
 	// Check whether the user is logged in
 	else if (!Csession.userID)
 		res.redirect("/login");
-	// Init webpage
-	res.write(fs.readFileSync("./pages/article/article.html"));
 	/**
 	 * @type {{name: string, content: string, views: number, author: string, votes: number}[]}
 	 */
 	let article = [];
-	// Init hidden data
-	res.write(`
-        <span style="display: none">${Csession.userID}</span>
-        <span style="display: none">Other Articles</span>
-    `)
-	// All lists of articles
-	res.write(SortByComponent(true));
-	// Header
-	res.write(Header("Other Articles"));
-	// Articles
-	res.write("<div id='created-article'>");
 	for (let i of r) {
 		if (i.user !== Csession.userID) {
 			article.push({
-				content: ArticleTemplate(i),
 				views: i.views ?? 0,
 				author: i.user,
 				name: i.name,
@@ -111,15 +74,9 @@ app.get("/otherarticle", async (req, res) => {
 
 	// Init articles
 	article = InitCategory("views", article);
-	for (let atc of article)
-		res.write(atc.content);
-	// Close created article div
-	res.write("</div>");
-	res.write(`
-        <span style="display: none">${article.map(e => e.name)}}</span>
-    `)
-    // Load javascript in webpage
-    res.write(`<script src="/javascripts/homepage/links.js"></script>`)
-	res.write(`<script src="/javascripts/homepage/navbuttons.js"></script>`);
-	res.end('<script src="/javascripts/homepage/endscript.js"></script>');
+	return next.render(req, res, "/article/article", {
+        Csession: Csession,
+        headerName: "Other Articles",
+        articles: article
+    });
 });
--- routes/article/homepage.mjs
+++ routes/article/homepage.mjs
@@ -1,10 +1,8 @@
 import {
     DB,
-    ArticleTemplate,
-    Header,
     InitCategory,
-    SortByComponent
 } from "../../resource/resource.mjs";
+import { next } from "../../app/next.mjs";
 import app from "../../app/config.mjs";
 import fs from "fs";
 
@@ -16,46 +14,27 @@ app.get("/article", async (req, res) => {
     Csession = req.session;
 	// Search all articles which belongs to current user
 	const r = await DB.sites.find({});
-    res.write(fs.readFileSync("./pages/article/article.html").toString().trim().replace(/\<\/html\>/, "").replace(/\<\/body\>/, ""));
     /**
      * @type {{name: string, content: string, views: number, author: string, votes: number}[]}
      */
     let article = [];
-    // Init hidden data
-    res.write(`
-        <span style="display: none">${Csession.userID}</span>
-        <span style="display: none">Discover</span>
-    `)
-    // All lists of articles link
-    res.write(SortByComponent(!!Csession.userID));
-    // Header
-    res.write(Header("Discover"));
     // Created articles
-    res.write("<div id='created-article'>");
     for (let i of r) {
         article.push({
             name: i.name,
-            content: ArticleTemplate(i),
             views: i.views ?? 0,
             author: i.user,
             votes: i.votes,
             data: i
         });
     }
-
-    // Init articles
+    // Sort articles
     article = InitCategory("views", article);
-    for (let atc of article)
-        res.write(atc.content);
-    // Close created article div
-    res.write("</div>");
-    res.write(`
-        <span style="display: none">${article.map(e => e.name)}</span>
-    `)
-    // Load javascript in webpage
-    res.write(`<script src="/javascripts/homepage/links.js"></script>`)
-    res.write(`<script src="/javascripts/homepage/navbuttons.js"></script>`);
-    res.end('<script src="/javascripts/homepage/endscript.js"></script>');
+    return next.render(req, res, "/article/article", {
+        Csession: Csession,
+        headerName: "Discover",
+        articles: article
+    });
 });
 
 // Most Vote
@@ -65,26 +44,13 @@ app.get("/mostvote", async (req, res) => {
     Csession = req.session;
 	// Search all articles which belongs to current user
 	const r = await DB.sites.find({});
-    res.write(fs.readFileSync("./pages/article/article.html").toString().trim().replace(/\<\/html\>/, "").replace(/\<\/body\>/, ""));
     /**
      * @type {{content: string, views: number, author: string, votes: number}[]}
      */
     let article = [];
-    // Init data
-    res.write(`
-        <span style="display: none">${Csession?.userID}</span>
-        <span style="display: none">Most Voted</span>
-    `)
-    // All lists of articles link
-    res.write(SortByComponent(!!Csession.userID))
-    // Header
-    res.write(Header("Most Voted"));
-    // Created article
-    res.write("<div id='created-article'>");
     // Add articles from database
     for (let i of r) {
         article.push({
-            content: ArticleTemplate(i),
             views: i.views ?? 0,
             author: i.user,
             votes: i.votes,
@@ -95,17 +61,9 @@ app.get("/mostvote", async (req, res) => {
 
     // Init articles
     article = InitCategory("votes", article);
-    for (let atc of article)
-        res.write(atc.content);
-    // End of articles block
-    res.write("</div>");
-    res.write(`
-        <span style="display: none">${article.map(e => e.name)}}</span>
-    `)
-    // Load javascript in webpage
-    res.write(`<script src="/javascripts/homepage/links.js"></script>`)
-    // Load javascript in webpage
-    res.write(`<script src="/javascripts/homepage/navbuttons.js"></script>`);
-    // End scripts
-    res.end('<script src="/javascripts/homepage/endscript.js"></script>');
+    return next.render(req, res, "/article/article", {
+        Csession: Csession,
+        headerName: "Most Voted",
+        articles: article
+    });
 });
