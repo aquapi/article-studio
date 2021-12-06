885ad8cdea525d43280cb82ff470c09ae9080cd8
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-06 09:29:20 +0700

    App configs

--- app/config.mjs
+++ app/config.mjs
@@ -6,6 +6,12 @@ import * as path from "path";
 import https from "https";
 import fs from "fs";
 import { Server } from "socket.io";
+import createMemoryStore from "memorystore";
+
+// Memory store
+const MemoryStore = createMemoryStore(session);
+
+// Load ENV
 config();
 
 // create app
@@ -16,6 +22,9 @@ export const server = https.createServer({
     key: fs.readFileSync("ssl/key.pem"),
     cert: fs.readFileSync('ssl/cert.pem')
 }, app);
+
+// Trust proxy
+app.set('trust proxy', 1);
 
 // Socket
 export const socket = new Server(server);
@@ -41,8 +50,14 @@ app.use(express.static(path.resolve()));
 app.use(session({
     secret: process.env.SESSION_SECRET,
     saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 },
-    resave: false
+    cookie: { 
+        maxAge: 1000 * 60 * 60 * 24,
+        secure: true
+    },
+    resave: false,
+    store: new MemoryStore({
+        checkPeriod: 86400000 // prune expired entries every 24h
+    })
 }));
 
 // Execute when error detected
