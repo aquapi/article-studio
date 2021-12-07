efcb1078131b6ddfa4f00ab328e5d46de017ffcb
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-07 16:28:12 +0700

    SSL Certificate

--- ssl/SSL.bat
+++ ssl/SSL.bat
@@ -4,5 +4,3 @@ call openssl genrsa -out ssl\key.pem
 call openssl req -new -key ssl\key.pem -out ssl\csr.pem
 call openssl x509 -req -days 9999 -in ssl\csr.pem -signkey ssl\key.pem -out ssl\cert.pem
 del ssl\csr.pem
-
-echo Generate Successfully
