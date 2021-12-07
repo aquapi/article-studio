0a7c8006cb98ea11c4c8b57549928a174cf2b03f
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-07 16:40:55 +0700

    Delete SSL Directory

--- ssl/SSL.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-call openssl genrsa -out ssl\key.pem
-call openssl req -new -key ssl\key.pem -out ssl\csr.pem
-call openssl x509 -req -days 9999 -in ssl\csr.pem -signkey ssl\key.pem -out ssl\cert.pem
-del ssl\csr.pem
