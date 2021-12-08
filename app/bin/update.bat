248b55d42847e72b4db5361bcd7b06bc1ace3a38
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-08 16:56:14 +0700

    App

--- app/bin/update.bat
+++ app/bin/update.bat
@@ -3,5 +3,3 @@
 call npm i nodemailer mongoose express-session express dotenv cookie-parser next nodemon
 call npm i @types/cookie-parser @types/express @types/express-session @types/nodemailer --save-dev
 call npm fund
-
-echo Update Success
