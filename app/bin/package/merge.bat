a26c0084ea32a76fc41272b82d686b41262dae1d
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-08 08:42:48 +0700

    App configs

--- app/bin/package/merge.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call git merge 
-echo Merge Complete
--- app/bin/package/update.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call npm i nodemailer mongoose express-session express dotenv cookie-parser next nodemon
-call npm i @types/cookie-parser @types/express @types/express-session @types/nodemailer --save-dev
-call npm fund
-
-echo Update Success
