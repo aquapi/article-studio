2e4b6ffc77a98c5063072e0da2467bcac9cbb735
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-08 08:35:39 +0700

    Move BIN to App folder

--- bin/run/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-call npx next build
-call nodemon --config nodemon.json server.mjs
--- bin/ssl/SSL.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-openssl genrsa -out ssl/key.pem
-openssl req -new -key ssl/key.pem -out ssl/csr.pem
-openssl x509 -req -days 9999 -in ssl/csr.pem -signkey ssl/key.pem -out ssl/cert.pem
-rm ssl/csr.pem
