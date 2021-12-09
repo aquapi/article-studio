50b0cda5be0f527d3b37a682a209a9b6e7b8a49b
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-09 17:21:40 +0700

    Server

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import { hostname, port } from "./app/resource.mjs";
 import server from "./app/server.mjs";
 import './app/app.mjs';
 
+// Start the server
 server.listen(port, hostname, () => {
     console.log("Server is listening on port " + port + ", address https://" + hostname);
 });
