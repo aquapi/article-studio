e52c34c8283d587bce8bfc930d2640e1192b7876
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-10 11:05:35 +0700

    Server

--- index.mjs
+++ index.mjs
@@ -1,8 +1,8 @@
 import { hostname, port } from "./app/resource.mjs";
-import server from "./app/server.mjs";
-import './app/app.mjs';
+import start from "./app/servers/start.mjs";
 
 // Start the server
-server.listen(port, hostname, () => {
-    console.log("Server is listening on port " + port + ", address https://" + hostname);
-});
+await start();
+
+// Server data
+console.log("Server is listening on port " + port + ", address https://" + hostname);
