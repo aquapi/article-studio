b0e1bb589741ef95e2e82fe4906f545ec4956dd4
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-13 09:10:40 +0700

    Server

--- index.mjs
+++ index.mjs
@@ -1,14 +1,20 @@
-import { hostname, port } from "./app/resource.mjs";
+import { config } from "dotenv";
 import target from "./app/servers/server.mjs";
 import HttpsServer from "./app/dependencies/HttpsServer.mjs";
-import "./app/app.mjs";
+import "./app/load.mjs";
+
+// Load ENV
+config();
 
 // Server
-const server = new HttpsServer(target, port, hostname);
+const server = new HttpsServer(
+    target, Number(process.env.PORT) || 443, 
+    process.env.LAN_IP || "0.0.0.0"
+);
 
 // Start the server
 await server.start();
 
 // Print server data
-console.log("Server is listening on port " + server.port + ", address https://" + server.hostname)
+console.log("Server is listening on port " + server.port + ", address https://" + server.hostname);
 
