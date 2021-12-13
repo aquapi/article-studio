c49b8242ea2f97ae4469ccf1390a2345cfb5cf74
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-13 17:26:59 +0700

    Routes

--- routes/account/login.mjs
+++ routes/account/login.mjs
@@ -2,6 +2,10 @@ import { DB, transporter } from "../../app/resource.mjs";
 import app from "../../app/servers/express.mjs";
 import User from "../../models/user.mjs";
 import next from "../../app/servers/next.mjs";
+import createMailSender from "../../app/dependencies/SendMail.mjs";
+
+// Send mail
+const sendMail = createMailSender(transporter);
 
 /**
  * @type {string}
@@ -11,14 +15,14 @@ let CurrentUser;
 // Login page
 // https://localhost/login
 
-app.get("/login", (req, res) => 
+app.get("/login", (req, res) =>
 	req.session?.userID ? res.redirect("/article") : next.render(req, res, "/account/login")
 );
 
 // Sign up page
 // https://localhost/signup
 
-app.get("/signup", (req, res) => 
+app.get("/signup", (req, res) =>
 	req.session?.userID ? res.redirect("/article") : next.render(req, res, "/account/signup")
 );
 
@@ -51,24 +55,24 @@ app.post("/signupprocess", async (req, res) => {
 	const r = await DB.users.findOne({
 		username: req.body.name,
 	});
-	if (!r) {
-		transporter.sendMail({
-			from: 'aquaplmc@gmail.com',
-			to: req.body.email,
-			subject: 'Your username and password',
-			text: `
-				Username: ${req.body.name}
-				Password: ${req.body.pass}
-				If you didn't sign up on our site, just ignore or delete this mail
-				Send feedback to our site: Userfeedbackrespond@gmail.com
-			`
-		}, console.log);
-
-		await new User({
-			username: req.body.name,
-			password: req.body.pass,
-		}).save();
-	} 
+	if (!r) 
+		await Promise.all([
+			sendMail({
+				from: 'aquaplmc@gmail.com',
+				to: req.body.email,
+				subject: 'Your username and password',
+				text: `
+					Username: ${req.body.name}
+					Password: ${req.body.pass}
+					If you didn't sign up on our site, just ignore or delete this mail
+					Send feedback to our site: Userfeedbackrespond@gmail.com
+				`
+			}),
+			new User({
+				username: req.body.name,
+				password: req.body.pass,
+			}).save()
+		]);
 	if (!r || req.body.pass === r.password) {
 		req.session.userID = req.body.name;
 		CurrentUser = req.body.name;
