b2c66c479b06794a8db6558b77ad53b2e7dc43a9
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-15 09:57:47 +0700

    Routes

---
+++ routes/socket/connect.mjs
@@ -0,0 +1,11 @@
+// Socket server
+import { Server as SocketServer } from "socket.io";
+import chatMessage from "../../routes/socket/chat.mjs";
+import { target } from "../../app/servers/servers.mjs";
+
+// Socket.io server
+new SocketServer(target).on("connection", socket => {
+    // Chat event
+    socket.on('chat message', chatMessage(socket));
+});
+
