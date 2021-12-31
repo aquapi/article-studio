bf05746284313dd4a92acc6c584c21ff4cad9b0e
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-31 12:57:51 +0700

    App

--- app/servers/passport.mjs
+++ app/servers/passport.mjs
@@ -4,19 +4,20 @@ import { Strategy as LocalStrategy } from "passport-local";
 
 // Strategies
 passport.use(new LocalStrategy(
-    async (username, password, cb) => 
+    async (username, password, cb) =>
         cb(null, await DB.users.findOne({
             username,
             password,
         }) ?? false)
 ));
 
+// Middleware
 export default {
-    local: passport.authenticate("local", 
-		{
-			session: false,
-			failureMessage: "Invalid username or password",
-			failureRedirect: "/login"
-		}
-	),
+    login: passport.authenticate("local",
+        {
+            session: false,
+            failureMessage: "Invalid username or password",
+            failureRedirect: "/login"
+        }
+    )
 }
