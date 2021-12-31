08871df722c5c0cb061d91c469c32112c31c363d
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-31 12:40:23 +0700

    Server

--- index.mjs
+++ index.mjs
@@ -1,9 +1,6 @@
 import "./app/load.mjs";
-import { server } from "./app/servers/servers.mjs";
+import { target as server } from "./app/servers/servers.mjs";
 
-// Start the server
-await server.start();
-
-// Print server data
-console.log("Server is listening on port " + server.port + ", address https://" + server.hostname);
+// Listen to port 443
+server.listen(process.env.PORT || 443, "0.0.0.0");
 
