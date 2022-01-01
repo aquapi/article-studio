8a13c545efc64a96452e0ddf6b161640eca5e868
Revenity <87530663+aquapi@users.noreply.github.com>
2022-01-01 12:52:30 +0700

    Server

--- index.mjs
+++ index.mjs
@@ -1,5 +1,8 @@
 import "./app/load.mjs";
-import { target as server } from "./app/servers/servers.mjs";
+import { target as server } from "./app/loaders/servers.mjs";
+
+// Prevent memory leaks
+process.setMaxListeners(0);
 
 // Listen to port 443
 server.listen(process.env.PORT || 443, "0.0.0.0");
