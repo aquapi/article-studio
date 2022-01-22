f4a79e0687029d1bacddcae24cc92db80f554cc1
Revenity <87530663+aquapi@users.noreply.github.com>
2022-01-22 13:55:07 +0700

    App

--- app/bin/update.bat
+++ app/bin/update.bat
@@ -4,3 +4,4 @@ call npm i cookie-parser dotenv express express-session hpp memorystore
 call npm i mongoose next nodemailer nodemon passport passport-local react react-dom socket.io
 call npm i @types/cookie-parser @types/express @types/express-session @types/hpp @types/nodemailer --save-dev
 call npm fund
+call npm audit fix --force
--- app/bin/update.sh
+++ app/bin/update.sh
@@ -4,3 +4,4 @@ npm i cookie-parser dotenv express express-session hpp memorystore
 npm i mongoose next nodemailer nodemon passport passport-local socket.io react react-dom
 npm i @types/cookie-parser @types/express @types/express-session @types/hpp @types/nodemailer --save-dev
 npm fund
+npm audit fix --force
