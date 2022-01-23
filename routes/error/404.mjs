01a7c773672ade6948c94c283d26738f857ddd25
Revenity <87530663+aquapi@users.noreply.github.com>
2022-01-23 20:56:03 +0700

    Finish

--- routes/error/404.mjs
+++
@@ -1,5 +0,0 @@
-import app from "../../app/loaders/express.mjs";
-
-// 404
-app.get("/*", (_, res) => res.redirect("/article"));
-   
--- routes/process/logout.mjs
+++
@@ -1,9 +0,0 @@
-import app from "../../app/loaders/express.mjs";
-
-// Log out
-app.get("/logout",
-	(req, res) => {
-		req.session.destroy();
-		res.redirect("/article");
-	}
-);
