2fcb8bd5358dd1597365c621e46e2dcb44312c3b
Revenity <87530663+aquapi@users.noreply.github.com>
2022-01-23 11:00:36 +0700

    Server

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import "./app/load.mjs";
-import { target as server } from "./app/loaders/servers.mjs";
+import server from "./app/loaders/server.mjs";
 
 // Listen to port 443 in HTTPS
 server.listen(process.env.PORT || 443);
