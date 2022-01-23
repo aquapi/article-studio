5c5cce528f9b1d7e9159b3284bd7f5e9df13f7b3
Revenity <87530663+aquapi@users.noreply.github.com>
2022-01-23 19:44:00 +0700

    Routes

--- routes/process/login.mjs
+++ routes/process/login.mjs
@@ -11,14 +11,17 @@ app.post(
                 if (err)
                     next(err);
                 // Check whether user not found
-                if (!user || req.body.password !== user.password) {
-                    res.redirect("/login");
-                    return;
+                if (!user || req.body.password !== user.password)
+                    // 401 Unauthorized
+                    res.writeHead(401);
+                else {
+                    // Assign session when success
+                    req.session.userID = user.username;
+                    // 200 OK
+                    res.writeHead(200);
                 }
-
-                // Assign session when success
-                req.session.userID = user.username;
-                res.redirect("/article");
+                // End the response
+                res.end();
             }
         )(req, res, next)
 );
