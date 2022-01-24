1d2f018707029a228a218a799d208d91a688b21d
Revenity <87530663+aquapi@users.noreply.github.com>
2022-01-24 16:57:53 +0700

    Stylesheets, Images

--- public/javascripts/account/signup.js
+++ public/javascripts/account/signup.js
@@ -1,3 +1,20 @@
-document.getElementById("back").addEventListener("click", () => 
+document.getElementById("back").addEventListener("click", () =>
     location.replace(sessionStorage.getItem("prevLocation"))
 );
+
+document.querySelector("form").addEventListener("submit", async e => {
+    e.preventDefault();
+    // Send sign up request to /signupprocess
+    await axios.post("/signupprocess", {
+        name: document.querySelector("input[name='name']").value,
+        pass: document.querySelector("input[name='pass']").value,
+        email: document.querySelector("input[name='email']").value
+    })
+        // Check whether status equals 401
+        .then(res => res.status)
+        .catch(() => 401) === 401
+        // Failed
+        ? alert("Cannot sign up or log in")
+        // Go to previous location
+        : location.replace(sessionStorage.getItem("prevLocation"));
+});
