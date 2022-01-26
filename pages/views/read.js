ef36c780f3e3ac313127b1da5ba8644df7ce57c4
Revenity <87530663+aquapi@users.noreply.github.com>
2022-01-26 16:06:48 +0700

    Pages

--- pages/views/read.js
+++ pages/views/read.js
@@ -20,6 +20,17 @@ export default ({ name, admin_button, content, views, author, tag, votes, user }
         <div id="buttons">
             <button id="back">Back</button><br />
             <button id="discuss_redirect">Discuss</button><br />
+            <label htmlFor="theme-changer">Theme</label>
+            <select id="theme-changer">
+                <option id="base16/ocean">Default</option>
+                <option id="vs2015">VS Dark</option>
+                <option id="vs">VS Light</option>
+                <option id="github">Github</option>
+                <option id="idea">ItelliJ IDEA</option>
+                <option id="androidstudio">Android Studio</option>
+                <option id="atom-one-dark">Atom Dark</option>
+                <option id="atom-one-light">Atom Light</option>
+            </select>
         </div>
         {/*Content and article data*/}
         <div
