09cca200e651c1ee02e97b2a2002e0fd9ef7a347
Revenity <87530663+aquapi@users.noreply.github.com>
2022-01-29 17:22:56 +0700

    Stylesheets, Images, Javascripts, Icon

--- public/javascripts/content/coauth.js
+++ public/javascripts/content/coauth.js
@@ -1,15 +1,48 @@
-document.querySelector("form").addEventListener("submit", async e => {
-    e.preventDefault();
-    const newElement = document.createElement("li");
-    newElement.innerHTML = document.querySelector("input").value;
-    document.querySelector("ul").appendChild(newElement);
+// Save changes
+async function save() {
     await axios.post("/coauths/save/" + encodeURIComponent(
-        document.querySelector("h2").innerHTML
+        document.querySelector("title").innerHTML
             .slice(14)
             .replace('<!-- -->', '')
     ), {
-        coAuthor: Array.from(document.querySelectorAll("li")).map(v => v.innerHTML)
+        coAuthor: Array.from(document.querySelectorAll("li"))
+            .map(v => v.querySelector("span").innerHTML)
     }).then(v => v.status).catch(() => 403) === 403
         ? alert("Saved failed")
-        : alert("Save sucessful");
+        : void 0;
+}
+
+// Form event
+document.querySelector("form").addEventListener("submit", async e => {
+    e.preventDefault();
+    // Create new
+    const
+        newElement = document.createElement("li"),
+        newSpan = document.createElement("span"),
+        rmButton = document.createElement("button");
+    // Remove button
+    rmButton.className = "remove";
+    rmButton.innerHTML = "-";
+    // New span content
+    newSpan.innerHTML = document.querySelector("input").value;
+    // Add new span and button to li
+    newElement.appendChild(newSpan);
+    newElement.appendChild(rmButton);
+    // Append to list
+    document.querySelector("ul").appendChild(newElement);
+    // Save 
+    await save();
+    // Add listener
+    Array.from(document.getElementsByClassName("remove"))
+        .forEach(v => v.addEventListener("click", async () => {
+            document.querySelector("ul").removeChild(v.parentElement);
+            await save();
+        }))
 });
+
+// Remove button
+Array.from(document.getElementsByClassName("remove"))
+    .forEach(v => v.addEventListener("click", async () => {
+        document.querySelector("ul").removeChild(v.parentElement);
+        await save();
+    }))
