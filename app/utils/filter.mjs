75622469424462adfa45c1bbcfbffd83c629457a
Revenity <87530663+aquapi@users.noreply.github.com>
2022-04-03 17:51:40 +0700

    Utilities, loaders, routes, models

--- app/utils/filter.mjs
+++
@@ -1,6 +0,0 @@
-/**
- * @param {{name: string, content: string, views: number, author: string, votes: number, private: boolean}[]} articles
- */
-const filter = articles => articles.filter(v => !v.private);
-
-export default filter;
