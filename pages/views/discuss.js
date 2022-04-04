5614403fd70dc1529f00036bdd4eae5a05938983
Revenity <87530663+aquapi@users.noreply.github.com>
2022-04-04 13:32:25 +0700

    Pages

--- pages/views/discuss.js
+++ pages/views/discuss.js
@@ -1,11 +1,7 @@
 // @ts-check
 import Head from "../components/headers/discuss";
 import io from "socket.io-client";
-import { useState } from "react";
-
-// User ID if they haven't signed in
-const rnd = Math.round(Math.random() * 1000000);
-const socket = io("/discuss");
+import { useCallback, useEffect, useState } from "react";
 
 /**
  * @param {{name: string, user: string}} props
@@ -14,52 +10,76 @@ const socket = io("/discuss");
 export default ({ name, user }) => {
     // Messages
     const [messages, setMessages] = useState([]);
+    /**
+     * @type [import("socket.io-client").Socket , React.Dispatch<import("socket.io-client").Socket>]
+     */
+    const [socket, setSocket] = useState(null);
+
+    useEffect(() => {
+        setSocket(io("/discuss"));
+    }, [])
+
+    useEffect(() => {
+        // Handle socket
+        if (!socket) return;
+        socket.on('chat', (/** @type {string} */ msg, /** @type {string} */ anotherUser) => {
+            setMessages(
+                [...messages, {
+                    msg,
+                    textAlign: "left",
+                    user: anotherUser
+                }]
+            );
+        });
+        return () => socket 
+            ? void socket.off()
+            : null;
+    }, [socket, messages]);
 
     // Submit mesage
     /**
-     * @type {React.FormEventHandler<HTMLFormElement>} e
+     * @type {React.FormEventHandler<HTMLFormElement>} 
      */
-    const submitMessage = e => {
+    const submitMessage = useCallback(e => {
+        if (!socket) return;
+
         const input = e.currentTarget.querySelector("input");
         e.preventDefault();
-        if (input.value) {
-            /**
-             * @param {string} event Chat event
-             * @param {string} message Message
-             * @param {number} rnd User ID if user isn't logged in
-             * @param {string} CurrentUser Current user 
-             */
 
-            socket.emit('chat message', input.value, rnd, user);
-            setMessages(messages.concat({ msg: input.value, user: "Me", textAlign: "right" }));
-
-            // Reset input form after send message
-            input.value = '';
-        }
-    };
-
-    // Handle socket
-    // TODO: Validate whether anotherUser is equivalent to current user
-    socket.on('chat', (msg, anotherUser) => {
+        // Change messages 
         setMessages(
-            messages.concat({ msg, textAlign: "left", user: anotherUser })
+            [...messages, {
+                msg: input.value,
+                user: "Me",
+                textAlign: "right"
+            }]
         );
-    });
+
+        /**
+         * @param {string} event Chat event
+         * @param {string} message Message
+         * @param {number} rnd User ID if user isn't logged in
+         * @param {string} CurrentUser Current user 
+         */
+
+        socket.emit('chat message', input.value, user);
+    }, [socket, messages]);
 
     return <>
         <Head name={name} />
         {/*Messages will be here*/}
         <ul id="messages">{
-            messages.map(({ msg, textAlign, user: us }) =>
-                <li style={{ textAlign }} key={Date.now()}>
-                    <p>{msg}</p>
-                    <div className="username">{us}</div>
-                </li>
+            // Render error
+            messages.map(data =>
+                <li style={{ textAlign: data.textAlign }} key={Date.now()}>
+                    <p>{data.msg}</p>
+                    <div className="username">{data.user}</div>
+                </li>   
             )
         }</ul>
         {/*Send message form*/}
         <form id="form" onSubmit={submitMessage}>
-            <input id="input" autoComplete="off" placeholder="Enter your message here" />
+            <input id="input" autoComplete="off" placeholder="Enter your message here" required={true} />
             <button>Send</button>
         </form>
     </>;
