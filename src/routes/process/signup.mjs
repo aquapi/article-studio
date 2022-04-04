d261bf6afacb71fd83d458dba86c403c6669f9d6
Revenity <87530663+aquapi@users.noreply.github.com>
2022-04-04 22:14:38 +0700

    Utilities, loaders, routes, models

--- src/routes/process/signup.mjs
+++ src/routes/process/signup.mjs
@@ -1,11 +1,5 @@
 import app from "../../loaders/express.mjs";
-import passport from "passport";
 import User from "../../models/user.mjs";
-import { config } from "dotenv";
-import { createTransport } from "nodemailer";
-
-// Load env
-config();
 
 // Sign up process
 app.post("/signupprocess",
@@ -14,28 +8,6 @@ app.post("/signupprocess",
             username: req.body.name
         }).exec();
         if (!user) {
-            // Create a transporter
-            const transporter = createTransport({
-                service: 'gmail',
-                auth: {
-                    user: process.env.EMAIL,
-                    pass: process.env.PASSWORD
-                }
-            });
-
-            // Send mail to user
-            transporter.sendMail({
-                from: 'aquaplmc@gmail.com',
-                to: req.body.email,
-                subject: 'Your username and password',
-                text: `
-                    Username: ${req.body.name}
-                    Password: ${req.body.pass}
-                    If you didn't sign up on our site, just ignore or delete this mail
-                    Send feedback to our site: Userfeedbackrespond@gmail.com
-                `
-            });
-
             // Create new user and save to database
             await new User({
                 username: req.body.name,
