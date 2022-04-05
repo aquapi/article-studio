7e3f42f990e6069ddfba3eed397298b2f31f0962
Revenity <87530663+aquapi@users.noreply.github.com>
2022-04-05 16:29:37 +0700

    Pages

--- pages/views/profile.js
+++ pages/views/profile.js
@@ -1,40 +1,60 @@
 import Head from "../components/headers/profile";
+import { useState } from "react";
 
 /**
  * @param {{name: string, pass: string}}
  */
 export default ({ name, pass }) => {
-    return (
-        <>
-            <Head name={name} />
-            <div style={{ overflow: "hidden" }}>
-                {/*Go back to homepage*/}
-                <button id="back">Back</button>
-                {/*Form*/}
-                <form>
-                    <h3>Profile</h3>
-                    <hr />
-                    <input type="text" name="name" value={name} readOnly /><br />
-                    <div style={{ display: "flex", justifyContent: "center" }}>
-                        <input type="password" name="pass" value={pass} readOnly />
-                        {/*Display or hide password*/}
-                        <div id="display">
-                            <i className="fas fa-eye" style={{ cursor: "pointer" }}></i>
-                        </div>
+    // Is showing password
+    const [isShowing, setIsShowing] = useState(false);
+
+    // Delete account
+    const deleteAccount = async () => {
+        await fetch("/delete", {
+            method: "POST",
+            body: JSON.stringify({})
+        });
+        location.replace("/logout");
+    };
+
+    // Show password
+    const showPassword = () => {
+        setIsShowing(!isShowing);
+    };  
+
+    // Log out
+    const logout = () => location.replace("/logout");
+
+    return <>
+        <Head name={name} />
+        <div style={{ overflow: "hidden" }}>
+            {/*Go back to homepage*/}
+            <button id="back" onClick={
+                () =>
+                    location.href = sessionStorage.getItem("prevLocation") ?? "/article"
+            }>Back</button>
+
+            {/*Form*/}
+            <form>
+                <h3>Profile</h3>
+                <hr />
+                <input type="text" name="name" value={name} readOnly /><br />
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <input type={isShowing ? "text" : "password"} name="pass" value={pass} readOnly />
+
+                    {/*Display or hide password*/}
+                    <div id="display" onClick={showPassword}>
+                        <i className={isShowing ? "fas fa-eye-slash" : "fas fa-eye"} style={{ cursor: "pointer" }}></i>
                     </div>
-                </form>
-                {/*Buttons*/}
-                <div style={{ textAlign: "center" }}>
-                    <button id="logout">Logout</button>
-                    <button id="delete">Delete account</button>
-                    {/*Blank form to go to /delete using post method*/}
-                    <form id="blank" action="/delete" method="post"></form>
                 </div>
+            </form>
+            {/*Buttons*/}
+            <div style={{ textAlign: "center" }}>
+                <button id="logout" onClick={logout}>Logout</button>
+                <button id="delete" onClick={deleteAccount}>Delete account</button>
             </div>
-            <script type="text/javascript" src="/javascripts/getData.js"></script>
-            <script src="/javascripts/account/profile.js"></script>
-        </>
-    )
+        </div>
+    </>;
 };
 
 // Get props
