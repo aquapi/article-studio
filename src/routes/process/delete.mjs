58756863efa30000274e86cddf8d7d7306e034f0
Revenity <87530663+aquapi@users.noreply.github.com>
2022-04-05 16:29:53 +0700

    Utilities, loaders, routes, models

--- src/routes/process/delete.mjs
+++ src/routes/process/delete.mjs
@@ -5,27 +5,24 @@ import app from "../../loaders/express.mjs";
 // Delete an user
 app.post("/delete", async (req, res) =>
     req.session?.userID ?
-        (
-            await Promise.all([
-                Article.deleteMany({
-                    user: req.session.userID
-                }).exec(), 
-                User.deleteOne({
-                    username: req.session.userID
-                }).exec()
-            ]), 
-            res.redirect("/logout")
-        )
-    : res.redirect("/logout")
-); 
+        await Promise.all([
+            Article.deleteMany({
+                user: req.session.userID
+            }).exec(),
+            User.deleteOne({
+                username: req.session.userID
+            }).exec()
+        ])
+        : null
+);
 
 // Delete an article
-app.post("/article/delete", 
+app.post("/article/delete",
     async (req, res) => (
         await Article.deleteOne({
             user: req.session?.userID ?? "",
             name: req.body.name
-        }).exec(), 
-        res.redirect("/article") 
+        }).exec(),
+        res.redirect("/article")
     )
 );
